6011c10e2806908cabdddeb1e87155825e44dda2
Chiheb Ben Mahmoud <bmahmoud.chiheb@gmail.com>
2021-06-16 09:24:31 +0200

    2D line and 3D scatter plots support in info bar

--- src/info/table.ts
+++ src/info/table.ts
@@ -5,11 +5,21 @@
 
 import assert from 'assert';
 
-import { Property, Target } from '../dataset';
+import { Property, PropertyND, Target } from '../dataset';
 import { Indexes } from '../indexer';
+
+import { plot_propertiesND } from '../propertiesND/plotting_wrapper';
 
 interface TableProperty {
     values: number[] | string[];
+    cell: HTMLTableDataCellElement;
+}
+
+interface TablePropertyND {
+    values: number[][][];
+    xlabel?: string;
+    ylabel?: string;
+    zlabel?: string;
     cell: HTMLTableDataCellElement;
 }
 
@@ -21,6 +31,7 @@ export class Table {
 
     private _header: HTMLTableHeaderCellElement;
     private _properties: TableProperty[];
+    private _propertiesND: TablePropertyND[];
 
     /**
      * Create and append a new table inside the given `HTMLElement`.
@@ -29,12 +40,14 @@ export class Table {
      * @param target     is this table related to atom or structure
      * @param collapseID HTML id to use for the root div with class=collapse
      * @param properties properties to display in this table.
+     * @param properties2D proprties to plot in this table
      */
     constructor(
         root: HTMLElement,
         target: Target,
         collapseID: string,
-        properties: { [name: string]: Property }
+        properties: { [name: string]: Property },
+        propertiesND?: { [name: string]: PropertyND }
     ) {
         const template = document.createElement('template');
         template.innerHTML = `<div class="collapse chsp-info-table" id=${collapseID} data-parent='#info-tables'>
@@ -50,6 +63,7 @@ export class Table {
         this._header = group.querySelector('th') as HTMLTableHeaderCellElement;
         this._target = target;
         this._properties = [];
+        this._propertiesND = [];
 
         const tbody = group.querySelector('tbody') as HTMLTableSectionElement;
         for (const name in properties) {
@@ -83,6 +97,43 @@ export class Table {
                 values: properties[name].values,
             });
         }
+        // duplicated from previous section but modified for propertiesND
+        if (propertiesND !== undefined) {
+            for (const name in propertiesND) {
+                const tr = document.createElement('tr');
+                const td = document.createElement('td');
+
+                //  add the units to the property if it exists, this is identical to the _title in ../map/map.ts
+                const units = propertiesND[name].units;
+                let title = name;
+                if (units !== undefined) {
+                    title += `/${units}`;
+                }
+                td.innerText = title;
+
+                // add a tooltip containing the description of the property and underline if it exists
+                const description = propertiesND[name].description;
+                if (description !== undefined) {
+                    td.style.borderBottom = '1px dotted #00f';
+                    td.style.cursor = 'help';
+                    td.style.display = 'inline';
+                    td.title = description;
+                }
+
+                tr.appendChild(td);
+                const cell = document.createElement('td');
+                tr.appendChild(cell);
+
+                tbody.appendChild(tr);
+                this._propertiesND.push({
+                    values: propertiesND[name].values,
+                    xlabel: propertiesND[name].xlabel,
+                    ylabel: propertiesND[name].ylabel,
+                    zlabel: propertiesND[name].zlabel,
+                    cell: cell,
+                });
+            }
+        }
 
         this.show({ environment: 0, structure: 0, atom: 0 });
     }
@@ -93,7 +144,7 @@ export class Table {
      */
     public show(indexes: Indexes): void {
         let displayId;
-        let index;
+        let index: number;
         if (this._target === 'structure') {
             displayId = indexes.structure + 1;
             index = indexes.structure;
@@ -107,6 +158,24 @@ export class Table {
         this._header.innerText = `Properties for ${this._target} ${displayId}`;
         for (const s of this._properties) {
             s.cell.innerText = s.values[index].toString();
+        }
+        for (const s of this._propertiesND) {
+            s.cell.innerHTML = '<button id="btn" name="draw me"> Draw me! </button>';
+            const draw_button = document.getElementById('btn') as HTMLElement;
+            draw_button.onclick = function draw() {
+                s.cell.innerHTML = '<div id="plot"></div>';
+                const enviro = document.getElementById('plot') as HTMLElement;
+                plot_propertiesND(
+                    s.values[index][0],
+                    s.values[index][1],
+                    s.values[index][2],
+                    enviro,
+                    `index ${index + 1}`,
+                    s.xlabel,
+                    s.ylabel,
+                    s.zlabel
+                );
+            };
         }
     }
 }
