0c5bc70f3f212bc5ba69cf06290e5dc4d4b79aa6
Jakub Lála <jakublala@gmail.com>
2021-07-09 14:23:44 +0200

    temp commit

--- tests/map/map.test.ts
+++ tests/map/map.test.ts
@@ -1,15 +1,12 @@
-// Initialize JSDOM setup
-import { default as setupJSDOM } from '../jsdom';
-setupJSDOM();
-
-// Add relevant classes/functions
 import { PropertiesMap } from '../../src/map';
 import { EnvironmentIndexer } from '../../src/indexer';
 import { Target } from '../../src/dataset';
 import { getByID } from '../../src/utils';
+import { AxisOptions } from '../../src/map/options';
 
 import { assert } from 'chai';
-import { AxisOptions } from '../../src/map/options';
+
+let documentHTML: string;
 
 const DUMMY_PROPERTIES = {
     first: {
@@ -70,45 +67,45 @@ const DUMMY_STRUCTURES = [
 ];
 
 describe('Map', () => {
+    before(() => {
+        // store karma's default HTML
+        documentHTML = document.body.innerHTML;
+    });
+
     it('can remove itself from DOM', () => {
         const root = document.createElement('div');
-        document.body.appendChild(root);
-        root.id = 'mapID';
         const indexer = new EnvironmentIndexer('structure', DUMMY_STRUCTURES);
         const map = new PropertiesMap(
-            { id: 'mapID', settings: DUMMY_MAP_SETTINGS },
+            { element: root, settings: DUMMY_MAP_SETTINGS },
             indexer,
             DUMMY_PROPERTIES
         );
-        console.log('after PropertiesMap');
 
         assert(root.innerHTML !== '');
         assert(document.body.innerHTML !== '');
 
         map.remove();
         assert(root.innerHTML === '');
-        document.body.removeChild(root);
+
         // remove SVG element created by Plotly
         document.getElementById('js-plotly-tester')?.remove();
-        assert(document.body.innerHTML === '');
+        assert(document.body.innerHTML === documentHTML);
     });
 
-    // it('plotted property changes when changed in map settings', () => {
-    //     const root = document.createElement('div');
-    //     document.body.appendChild(root);
-    //     root.id = 'mapID';
-    //     const indexer = new EnvironmentIndexer('structure', DUMMY_STRUCTURES);
-    //     const map = new PropertiesMap({ id: 'mapID', settings: {} }, indexer, DUMMY_PROPERTIES);
+    it('plotted property changes when changed in map settings', () => {
+        const root = document.createElement('div');
+        const indexer = new EnvironmentIndexer('structure', DUMMY_STRUCTURES);
+        const map = new PropertiesMap({ element: root, settings: {} }, indexer, DUMMY_PROPERTIES);
 
-    //     checkPropertySelect(map['_options'].x, 'x');
-    //     checkPropertySelect(map['_options'].y, 'y');
-    //     checkPropertySelect(map['_options'].z, 'z');
+        checkPropertySelect(map['_options'].x, 'x');
+        checkPropertySelect(map['_options'].y, 'y');
+        checkPropertySelect(map['_options'].z, 'z');
 
-    //     function checkPropertySelect(axisOptions: AxisOptions, axisName: string) {
-    //         const selectElement = getByID<HTMLSelectElement>(`chsp-${axisName}`);
-    //         selectElement.value = 'second';
-    //         selectElement.dispatchEvent(new Event('change'));
-    //         assert(axisOptions.property.value === 'second');
-    //     }
-    // });
+        function checkPropertySelect(axisOptions: AxisOptions, axisName: string) {
+            const selectElement = getByID<HTMLSelectElement>(`chsp-${axisName}`);
+            selectElement.value = 'second';
+            selectElement.dispatchEvent(new window.Event('change'));
+            assert(axisOptions.property.value === 'second');
+        }
+    });
 });
