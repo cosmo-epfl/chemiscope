b418cdef51c6c3b7e92ebf5a8050343cdc6ece71
Luthaf <luthaf@luthaf.fr>
2021-07-14 17:20:09 +0200

    Fix reading version number from symlink on windows

--- python/setup.py
+++ python/setup.py
@@ -5,12 +5,29 @@ import os
 
 # TODO: build the javascript code when creating a sdist
 
-if __name__ == "__main__":
-    # get the version directly from the package.json file
+
+def read_version_from_package_json():
+    """get the version number directly from the package.json file"""
     package_json = os.path.join(os.path.dirname(__file__), "package.json")
-    version = json.load(open(package_json))["version"]
+    if os.path.islink(package_json):
+        # unix, symlink exists
+        with open(package_json) as fd:
+            return json.load(fd)["version"]
+    else:
+        # windows, git represent symlinks as file containing the actual path
+        with open(package_json) as fd:
+            package_json_path = fd.read().strip()
+
+        package_json_path = os.path.join(
+            os.path.dirname(package_json), package_json_path
+        )
+        with open(package_json_path) as fd:
+            return json.load(fd)["version"]
+
+
+if __name__ == "__main__":
     setup(
-        version=version,
+        version=read_version_from_package_json(),
         data_files=[
             # like `jupyter nbextension install --sys-prefix`
             (
